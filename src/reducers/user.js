dcd91c882b309b63eb259cde0cf039d0cdecd6bb
FabioMartins <fabbio1000@gmail.com>
2022-05-23 14:32:18 -0300

    ajustando pastas para corrigir erro do teste

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,5 +1,5 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
-import { ADD_USER } from '../actions';
+import { ADD_USER } from '../actions/index';
 
 const INITIAL_STATE = {
   email: '',
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,5 +1,5 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
-import { ADD_OPERATION } from '../actions';
+import { ADD_OPERATION } from '../actions/index';
 
 const INITIAL_STATE = {
   currencies: [],
